a242b4e602959f7575ad3b3303f07d607f49d340
ecanals@github.com <enriquecanals@gmail.com>
2013-05-28 00:52:44 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailit::Application.config.secret_token = '012ad8406971354d8d9e5ab8a49021d881cce4dad189af861d3df733cc677fdfffdf5df6abc9318c8800900fe5acdff8946ada23d51bafde84ac2c9298542596'
